395b200cd31b6ea8c1f4e1348fd28c4ce5576095
georg <PuuRaidur@users.noreply.github.com>
2025-11-10 03:19:08 +0200

    Some fixes

--- src/main/java/com/koodjohvi/movieapi/repositories/MovieRepository.java
+++ src/main/java/com/koodjohvi/movieapi/repositories/MovieRepository.java
@@ -4,6 +4,7 @@ import com.koodjohvi.movieapi.entities.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,10 +13,12 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     // custom query methods as mandatory
     List<Movie> findByGenresId(Long genreId);
+    @Query("SELECT m FROM Movie m WHERE m.releaseYear = :year")
     List<Movie> findByReleaseYear(Integer year);
     List<Movie> findByActorsId(Long actorId);
     List<Movie> findByTitleContainingIgnoreCase(String title);
     Page<Movie> findByGenresId(Long genreId, Pageable pageable);
+    @Query("SELECT m FROM Movie m WHERE m.releaseYear = :year")
     Page<Movie> findByReleaseYear(Integer year, Pageable pageable);
     Page<Movie> findByActorsId(Long actorId, Pageable pageable);
     Page<Movie> findByTitleContainingIgnoreCase(String title, Pageable pageable);
